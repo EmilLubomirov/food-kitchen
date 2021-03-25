5b4072e4c8b1ee56325dea136220e0d47752a04f
EmilLubomirov <emil_lubomirov_1999@abv.bg>
2021-03-25 17:18:26 +0200

    Initial commit

---
+++ food-kitchen-backend/src/main/java/com/example/foodkitchen/FoodKitchenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodKitchenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodKitchenBackendApplication.class, args);
+    }
+
+}
---
+++ food-kitchen-backend/src/test/java/com/example/foodkitchen/FoodKitchenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodKitchenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
